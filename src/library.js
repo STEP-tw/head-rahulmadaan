bae484307720464fa1bd785175af32e60b6069c8
Rahul <44020002+rahulmadaan@users.noreply.github.com>
2018-12-07 09:53:06 +0530

    Added error handing

--- src/library.js
+++ src/library.js
@@ -1,4 +1,4 @@
-const extractUsefulContent = function(content,type='1',limit) {
+const extractUsefulContent = function(content,limit,type='1') {
   if(type == '1') {
   return getLines(content,limit);
   }
@@ -16,7 +16,7 @@ const getCharacters = function(content,limit=0){
 const makeHeader = function(head) {
   return "==> "+head+" <==";
 };
-
+// ['-n6%']
 const extractNumber = function(inputParameters) {
   let string = inputParameters.join('');
   if(string.includes('-c') && !string.match(/[0-9]/)) {
@@ -31,7 +31,7 @@ const extractNumber = function(inputParameters) {
     string = inputParameters.join('');
     string = string.slice(index);
   }
-  return Math.abs(parseInt(string)) || 10;
+  return parseInt(string) || 10;
 }
 
 const getFileNames = (x=>x.filter(file => file.includes('.') || file.includes('_')));
@@ -40,10 +40,10 @@ const getTypeAndLength = (x=>x.filter(file => !file.includes('.')));
 const extractType = function(input) {
   input = input.join('');
   if(input.includes('-c')) { return '0';}
-  return 1;
+  return  1;
 }
 const classifyInput = function(userInput) {
-  let file = getFileNames(userInput);
+  let file  = getFileNames(userInput);
   let limit = extractNumber(getTypeAndLength(userInput));
   let type = extractType(getTypeAndLength(userInput));
   return {file:file, extractNumber:limit, type:type};
@@ -54,6 +54,10 @@ const head = function(userInput=[],fs) {
   let delimiter = '';
   let text = '';
   let {file,extractNumber,type} = classifyInput(userInput);
+
+  let error = checkErrors(file,type,extractNumber);
+  if(error) {return error;}
+
   for(let count=0; count<file.length; count++) {
     if(!fs.existsSync(file[count])) {
       data.push('head: '+ file[count]+': No such file or directory'); count++; 
@@ -63,11 +67,23 @@ const head = function(userInput=[],fs) {
       data.push(delimiter + makeHeader(file[count]));
       delimiter = '\n'
     }
-    text = (fs.readFileSync(file[count],'utf8'));
-    data.push(extractUsefulContent(text,type,extractNumber));
+    text = fs.readFileSync(file[count],'utf8');
+    data.push(extractUsefulContent(text,extractNumber,type));
   }
   return data.join('\n');
+} 
+
+const checkErrors = function(fileName,type,value) {
+  let invalidValue = (value<=0 || isNaN(value));
+  if(invalidValue && type == '1') {
+    return 'head: illegal line count -- '+value;
+  }
+  if(invalidValue && type == '0') {
+    return 'head: illegal byte count -- '+value;
+  }
+  // return 0; 
 }
+
 module.exports={
   extractUsefulContent,
   extractNumber,
