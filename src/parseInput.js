d702254b2658f9ffd33e61509946449895c3d229
Rahul Madaan <44020002+rahulmadaan@users.noreply.github.com>
2018-12-20 00:52:28 +0530

    refactored input functions from parseInput library

--- src/parseInput.js
+++ src/parseInput.js
@@ -36,4 +36,23 @@ const extractType = function (userInput) {
     }
     return '1';
 };
-module.exports = {makeHeader,extractNumber,getFileNames,getOptionAndNumber,extractType}
+
+const parseUserInput = function (userInput) {
+    let result = {};
+    result.files = userInput.filter(file => !file.startsWith("-") && !file.match(/^\d/));
+    result.options = userInput.slice(0, userInput.length - result.files.length);
+    return result;
+};
+
+
+const classifyInput = function (userInput) {
+    let input = parseUserInput(userInput);
+    let file = input.files;
+    let optionAndNumber = input.options;
+    let number = extractNumber(optionAndNumber) || 10;
+    let type = extractType(optionAndNumber);
+
+    return { file, number, type };
+};
+
+module.exports = { makeHeader, extractNumber, getFileNames, getOptionAndNumber, extractType, classifyInput }
